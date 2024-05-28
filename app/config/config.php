92124e52733fe60309a491800a51392952e085ca
Wrance <clemenrancet@gmail.com>
2024-05-28 09:59:05 +0200

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+// setting time zone
+date_default_timezone_set("Asia/Kolkata");
+
+// define the site root
+define('SITE_ROOT', 'http://stock-lb-1576783510.us-east-1.elb.amazonaws.com/ample/');
+
+
+
+// Database Information
+// Database Hostname
+define('DATABASE_HOST','stock-lb-1576783510.us-east-1.elb.amazonaws.com');
+// Database Username
+define('DATABASE_USER','admin');
+// Database Name
+define('DATABASE_NAME', 'ample');
+// Database DB_PASS
+define('DATABASE_PASS','Training123');
+
+
+ ?>
